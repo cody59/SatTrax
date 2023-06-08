310c726f863aea46b33b5cc9e69b86b21a1a7554
taken-bacon <131088657+taken-bacon@users.noreply.github.com>
2023-06-08 10:25:37 -0400

    basic organization of data

--- tracker.py
+++ tracker.py
@@ -1,21 +1,38 @@
 #rules for parsing the data
 
-Master_Dictionary={a:[b,c]} #created to be able to pull any information from
-#a is the name of the sattilite, b and see are lines one and two
+Master_Dictionary={} #created to be able to pull any information from
+name=[]
+values=[]
+#a is the name of the sattilite, b and c are lines one and two
 
 
 
 #data tokenization
-list=["""AAAAAAAAAAAAAAAAAAAAAAAA
+list="""AAAAAAAAAAAAAAAAAAAAAAAA
 1 NNNNNU NNNNNAAA NNNNN.NNNNNNNN +.NNNNNNNN +NNNNN-N +NNNNN-N N NNNNN
-2 NNNNN NNN.NNNN NNN.NNNN NNNNNNN NNN.NNNN NNN.NNNN NN.NNNNNNNNNNNNNN"""]
+2 NNNNN NNN.NNNN NNN.NNNN NNNNNNN NNN.NNNN NNN.NNNN NN.NNNNNNNNNNNNNN""".split("\n")
+list1=[]
+list2=[]
+
+print(list)
+
 
 for i in list:
-    for n in range(len(i)):
-        if i[0] == "1":
-            holder=i.split()
-            
-        elif i[0] == "2":
-            
-        else:
-            print(str(i))
+    if i[0] == "1":
+        list.append(i)
+    elif i[0] == "2":
+        list2.append(i)
+    else:
+        name.append(i)
+print(list)
+
+for i in name:
+    Master_Dictionary.update({i:[]})
+for i in list1:
+    for n in Master_Dictionary:\
+        Master_Dictionary.update({n:[i]})
+for i in list1:
+    for n in Master_Dictionary:\
+        Master_Dictionary.update({n:[i]})
+
+print(list)
